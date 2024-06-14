957d2e8e035a697edd4fc5ddc6d9b2229296c510
EmileMarien <100191898+EmileMarien@users.noreply.github.com>
2024-06-14 22:05:17 +0200

    ud

--- pages/3_Database.py
+++ pages/3_Database.py
@@ -91,13 +91,12 @@ def get_consumption_data(db,meter_id):
     meter_ref = db.collection('meters').document(meter_id)
     consumption_data = meter_ref.get().to_dict().get('consumptiondata', [])
     # Iterate over each data point and extract timestamp and reading
-    st.write(consumption_data)
     for data_point in consumption_data:
         timestamp = data_point['timestamp']
         reading = data_point['reading']
         data_points.append((timestamp, reading))
 
-    st.write(f"Fetched {len(data_points)} data points for Meter {meter_id}")
+    st.write(f"Fetched {data_points} data points for Meter {meter_id}")
     # Convert data_points to pandas DataFrame
     if data_points:
         df = pd.DataFrame(data_points, columns=['timestamp', 'reading'])
