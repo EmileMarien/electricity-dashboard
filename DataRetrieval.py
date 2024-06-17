f41d76d92b92d8fc46c1341aadb2d68e30c1cd3b
EmileMarien <100191898+EmileMarien@users.noreply.github.com>
2024-06-17 13:03:45 +0200

    update

--- DataRetrieval.py
+++ DataRetrieval.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
+from google.cloud import firestore
 
 def fetch_electricity_prices():
     """
@@ -38,7 +39,6 @@ def fetch_electricity_prices():
     datetimes = []
     prices = []
     
-
     rows = table.find_all('tr', class_='dxgvDataRow_Office2010Blue')
     if rows is None or len(rows) == 0:
         return "No data rows found in the table"
@@ -57,6 +57,66 @@ def fetch_electricity_prices():
     
     return df
 
-# Example usage
-if __name__ == "__main__":
-    df = fetch_electricity_prices
+
+def add_belpex_to_firestore(belpex, db):
+    """
+    Adds the BELPEX prices to Firestore for the specified meter_id.
+
+    Args:
+        belpex (pd.DataFrame): DataFrame containing BELPEX prices.
+        meter_id (str): The ID of the meter to add the prices to.
+        db: Firestore client instance.
+
+    Returns:
+        str: A message indicating the number of data points added.
+    """
+
+    #latest_timestamp= get_latest_belpex_timestamp_from_firestore(db)
+    data_to_add = []
+
+    for index, row in belpex.iterrows():
+        timestamp_str = row['DateTime']
+        price_str = row['Price']
+
+        # Parse timestamp (adjust according to your specific datetime format)
+        timestamp = datetime.strptime(timestamp_str, '%d/%m/%Y %H:%M:%S')
+
+        # Check if timestamp is after the latest timestamp in Firestore
+        #if timestamp > latest_timestamp:
+            # Prepare data object
+        data = {
+            'timestamp': timestamp,
+            'value': float(price_str.replace('€', '').strip())  # Assuming price needs to be stored as a float
+        }
+        data_to_add.append(data)
+
+    # Update Firestore with new datapoints
+    if data_to_add:
+        prices_ref = db.collection('prices').document('belpex')
+        prices_ref.update({
+            'datapoints': firestore.ArrayUnion(data_to_add)
+        })
+
+    return f"Added {len(data_to_add)} new datapoints to Firestore under 'prices/belpex'"
+
+"""
+def get_latest_belpex_timestamp_from_firestore(db):
+    prices_ref = db.collection('prices').document('belpex')
+    doc = prices_ref.get()
+
+    if doc.exists:
+        data = doc.to_dict()
+        datapoints = data.get('datapoints', [])
+
+        if datapoints:
+            # Sort datapoints by timestamp in descending order to get the latest
+            datapoints_sorted = sorted(datapoints, key=lambda x: x['timestamp'], reverse=True)
+            return datapoints_sorted[0]['timestamp']
+        else:
+            # Return a default timestamp if no datapoints exist
+            return datetime.min
+    else:
+        # Handle case where document 'belpex' does not exist
+        return datetime.min
+"""
+
