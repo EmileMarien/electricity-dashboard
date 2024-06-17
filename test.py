566084249ec04a5bab3cd6455a22a0a8f88a26bd
EmileMarien <100191898+EmileMarien@users.noreply.github.com>
2024-06-17 14:43:00 +0200

    dd

--- test.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-st.write("Hello, Streamlit!")
-
-st.write("# ellowwww 👋")
-st.write("Welcome to the Electricity Meter Dashboard! 📊")
-st.sidebar.success("Select a demo above.")
