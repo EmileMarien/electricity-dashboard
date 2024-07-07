14e4358145fbffaea719845cc4a51d0daa51ad2f
EmileMarien <100191898+EmileMarien@users.noreply.github.com>
2024-07-08 01:06:52 +0200

    add pricefetching test

---
+++ test/test_pricefetching.py
@@ -0,0 +1,15 @@
+
+import unittest
+from context import fetch_electricity_prices
+class TestPriceFetching(unittest.TestCase):
+    def test_fetch_price(self):
+        print(fetch_electricity_prices())
+        self.assertEqual(fetch_electricity_prices().shape, (192, 1))
+        #check if numeric
+        self.assertTrue(fetch_electricity_prices()['Belpex'].dtype == 'float64')
+        #check if datetimeindex
+        self.assertTrue(fetch_electricity_prices().index.dtype == 'datetime64[ns]')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test/test_solarpowermodel.py
+++ test/test_solarpowermodel.py
@@ -1,6 +1,7 @@
 import unittest
 import pandas as pd
 from context import SolarPowerModel
+from context import fetch_electricity_prices
 
 class TestSolarPowerModel(unittest.TestCase):
     def test_set_solar_power(self):
@@ -56,6 +57,12 @@ class TestSolarPowerModel(unittest.TestCase):
         print(model.get_columns(columns=['Load_kW', 'PV_Power_kW', 'Belpex', 'GridFlow', 'DynamicTariff','GridFlow_Load','DynamicTariff_Load']))
         self.assertEqual(round(model.get_total_cost()), 1285)
 
+    def test_belpex_fetch_input(self):
+        model=SolarPowerModel()
+        model.set_belpex_df(fetch_electricity_prices())
+        print(model.get_columns(columns=['Belpex']))
+
+    # test calling SLP & SPP & when adding only single lines and converting to JSON
 
 if __name__ == '__main__':
     unittest.main()
