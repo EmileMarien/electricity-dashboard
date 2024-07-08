e0b237ac079e0cd3c616fb26238bd6dc62a891c3
EmileMarien <100191898+EmileMarien@users.noreply.github.com>
2024-07-08 21:43:16 +0200

    fetch SLP & SPP

--- features/solarpower/models/syntheticprofilefetching/syntheticprofilefetching.py
+++ features/solarpower/models/syntheticprofilefetching/syntheticprofilefetching.py
@@ -1,27 +1,24 @@
-
+from typing import List
+import pandas as pd
 
 def SLP_xls_to_pd(file_path: str) -> pd.DataFrame:
     """
     This function reads the SLP data from an Excel file and returns a DataFrame.
-
-    Args:
-    file_path (str): The path to the Excel file.
-
-    Returns:
-    pd.DataFrame: The DataFrame containing the SLP data.
     """
     # Read the Excel file
-    df = pd.read_excel(file_path)
+    df = pd.read_excel(file_path,sheet_name='ENU_UTC')
+    df['UTC'] = pd.to_datetime(df['UTC'])
 
     # Ensure the DataFrame has the correct columns
-    expected_columns = ['DateTime', 'Load_kW']
+    expected_columns = ['UTC', 'EN']
     if not all(col in df.columns for col in expected_columns):
         raise Exception(f"Columns in Excel file do not match expected columns: {expected_columns}")
 
     # Ensure the DateTime column is of type datetime
-    if df['DateTime'].dtype != 'datetime64[ns]':
+    if df['UTC'].dtype != 'datetime64[ns]':
         raise Exception("DateTime column is not of type datetime")
-
+    df=df[['UTC','EN']]
+    df.rename(columns={'UTC':'DateTime','EN':'Load_SLP_kW'},inplace=True)
     # Set the DateTime column as the index
     df.set_index('DateTime', inplace=True)
 
@@ -38,17 +35,19 @@ def SPP_xls_to_pd(file_path: str) -> pd.DataFrame:
     pd.DataFrame: The DataFrame containing the SPP data.
     """
     # Read the Excel file
-    df = pd.read_excel(file_path)
+    df = pd.read_excel(file_path,sheet_name='Ex-ante 2022 (IP8)')
+    df['UTC'] = pd.to_datetime(df['UTC'])
 
     # Ensure the DataFrame has the correct columns
-    expected_columns = ['DateTime', 'PV_Power_kW']
+    expected_columns = ['UTC', '5414488001704']
     if not all(col in df.columns for col in expected_columns):
         raise Exception(f"Columns in Excel file do not match expected columns: {expected_columns}")
 
     # Ensure the DateTime column is of type datetime
-    if df['DateTime'].dtype != 'datetime64[ns]':
+    if df['UTC'].dtype != 'datetime64[ns]':
         raise Exception("DateTime column is not of type datetime")
-
+    df=df[['UTC','5414488001704']]
+    df.rename(columns={'UTC':'DateTime','5414488001704':'PV_Power_SPP_kW'},inplace=True)
     # Set the DateTime column as the index
     df.set_index('DateTime', inplace=True)
 
