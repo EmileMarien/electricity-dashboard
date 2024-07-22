2979730ed1008f499c5312dba7fdee3ee92337e7
EmileMarien <100191898+EmileMarien@users.noreply.github.com>
2024-07-22 17:26:56 +0200

    ff

--- features/solarpower/states/solarpowerstate.py
+++ features/solarpower/states/solarpowerstate.py
@@ -53,7 +53,7 @@ class SolarPowerState():
         return None
     
     def update_belpex(self):
-        prices=fetch_electricity_prices()
+        prices=fetch_electricity_prices_xlsx()
         self.data_repository_belpex.update(prices)
         all_prices=self.data_repository_belpex.get_belpex()
 
