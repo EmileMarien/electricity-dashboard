80d13e0d35783f76042d264d081e298f739d2f67
Emile <emile.marien@gmail.com>
2025-12-26 12:13:38 +0100

    update

--- streamlit_app/pages/5_House_builder.py
+++ streamlit_app/pages/5_House_builder.py
@@ -2,7 +2,7 @@ import json
 import uuid
 import streamlit as st
 
-from components.three_builder_component import three_builder
+from elec_dashboard_components.three_builder_component import three_builder
 
 st.set_page_config(page_title="House Builder MVP", layout="wide")
 
